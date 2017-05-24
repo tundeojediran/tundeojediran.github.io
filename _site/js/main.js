fb17837022de02481462dd5c9b4df713d0830a09
dannytee <tundeojediran@gmail.com>
2017-05-24 08:42:02 +0100

    added base url

--- _site/js/main.js
+++ _site/js/main.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
 
-  if (window.location.pathname !== '' && window.location.pathname !== 'index.html') {
+  if (window.location.pathname !== '/tundeojediran/' && window.location.pathname !== '/tundeojediran/index.html') {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
 
